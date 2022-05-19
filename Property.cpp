cbd2e07f670c8c3df817e7170eb2136799e6a63f
it21281082 Weerasinghe <99232110+IT21281082@users.noreply.github.com>
2022-05-19 17:22:39 +0000

    IT21281082

---
+++ Property.cpp
@@ -0,0 +1,23 @@
+#include "Property.h"
+#include <cstring>
+
+Property::Property()
+{
+	propertyId = 0;
+}
+
+Property::Property(int pId)
+{
+    propertyId = pId;
+}
+void Property::checkAvailability()
+{
+
+}
+void Property::displayPropertyDetails()
+{
+}
+Property::~Property()
+{
+	//Destructor 
+}
