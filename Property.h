213340f9fe07e353dc6eae04f8091d2e694ba68e
it21281082 Weerasinghe <99232110+IT21281082@users.noreply.github.com>
2022-05-19 17:40:22 +0000

    IT21281082

---
+++ Property.h
@@ -0,0 +1,21 @@
+#include "selling.h"
+#include "buyer.h"
+#include "Admin.h"
+
+#define SIZE1 2
+#define SIZE2 2
+
+class Property
+{
+private:
+	int propertyId;
+
+public:
+  Property();
+	Property(int pId);
+	void checkAvailability();
+	void displayPropertyDetails();
+	~Property();
+
+};
+
